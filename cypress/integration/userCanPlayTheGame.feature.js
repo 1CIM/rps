44bc87bce7325125673e0a4bac9db062a4ff0b74
kim_haaga <kim.haaga@live.se>
2021-04-29 13:03:09 +0200

    Add game logic

--- cypress/integration/userCanPlayTheGame.feature.js
+++ cypress/integration/userCanPlayTheGame.feature.js
@@ -5,10 +5,24 @@ describe('User can play the game', () => {
     cy.visit('/')
   });
 
-  it('plays the game', () => {
+  it('plays and loose the game', () => {
     cy.get('[data-cy=game-container]').within(() => {
       cy.get('[data-cy=rock-btn]').click()
-      cy.get('[data-cy=result]').should('be.visible')
+      cy.get('[data-cy=result]').should('contain', 'You Lost')
+    })
+  });
+
+  it('plays and tie the game', () => {
+    cy.get('[data-cy=game-container]').within(() => {
+      cy.get('[data-cy=paper-btn]').click()
+      cy.get('[data-cy=result]').should('contain', 'Tie')
+    })
+  });
+
+  it('plays and win the game', () => {
+    cy.get('[data-cy=game-container]').within(() => {
+      cy.get('[data-cy=scissor-btn]').click()
+      cy.get('[data-cy=result]').should('contain', 'You Won')
     })
   });
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
 import React, { Component } from 'react'
 import {Button, Container, Header, Icon} from 'semantic-ui-react'
 
+const hands = ['rock', 'paper', 'scissor']
 class App extends Component {
+
+  state = {
+    p1: hands[0],
+    p2: hands[0],
+    winner: ""
+  }
+
+  play = () => {
+    this.setState({
+      p2: hands[Math.floor(Math.random() * hands.length)],
+      winner: this.selectWinner()
+    })
+  }
+
+  chooseHand = () => {
+    this.setState({
+      p1: hands
+    })
+  }
+
+  selectWinner = () => {
+    const { p1, p2 } = this.state
+
+    if (p1 === p2) {
+      return 'Tie'
+    } else if (
+      (p1 === 'rock' && p2 === 'scissor') ||
+      (p1 === 'scissor' && p2 === 'paper') ||
+      (p1 === 'paper' && p2 === 'rock')
+    ) {
+      return 'You Won'
+    } else {
+      return 'You Lost'
+    }
+  }
+
+
   render() {
+    const { winner } = this.state
     return (
       <>
         <Container>
-          <Header data-cy='game-header' >R.P.S</Header>
+          <Header as='h1' data-cy='game-header' >R.P.S</Header>
+          <Header data-cy='result' as='h3'>{winner ? this.selectWinner() : null}</Header>
           <Container data-cy='game-container'>
             <Button.Group>
-              <Button data-cy='rock-btn' value="rock">
+              <Button data-cy='rock-btn' value="rock" onClick={() => this.chooseHand[0], this.play }>
                 <Icon name='rock hand' size='huge' />
                 </Button>
-              <Button data-cy='scissor-btn' value="scissor">
+              <Button data-cy='scissor-btn' value="scissor" onClick={() => this.chooseHand[1], this.play }>
                 <Icon name='hand scissors' size='huge'/>
                 </Button>
-              <Button data-cy='paper-btn' value="paper">
+              <Button data-cy='paper-btn' value="paper" onClick={() => this.chooseHand[2], this.play }>
                 <Icon name='paper hand' size='huge'/>
                 </Button>
             </Button.Group>
