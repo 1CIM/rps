4382290502cad20e055bc89f2bb0e9a161574499
kim_haaga <kim.haaga@live.se>
2021-04-29 10:36:15 +0200

    Add semantic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {Container} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+      <Container>
+
+      </Container>
+      </>
     )
   }
 }
