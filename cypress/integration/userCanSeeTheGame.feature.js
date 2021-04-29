87e6c793011083ad871719b2b96ad8dcb3ba930a
kim_haaga <kim.haaga@live.se>
2021-04-29 12:26:16 +0200

    Add displaying of content

--- cypress/integration/userCanSeeTheGame.feature.js
+++ cypress/integration/userCanSeeTheGame.feature.js
@@ -7,8 +7,8 @@ describe('User can see the game board', () => {
     cy.get('[data-cy=game-header]').should('contain', 'R.P.S')
     cy.get('[data-cy=game-container]').within(() => {
       cy.get('[data-cy=rock-btn]').should('have.value', 'rock')
-      cy.get('[data-cy=paper-btn]').should('have.value', 'paper')
       cy.get('[data-cy=scissor-btn]').should('have.value', 'scissor')
+      cy.get('[data-cy=paper-btn]').should('have.value', 'paper')
     })
   });
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import {Container} from 'semantic-ui-react'
+import {Button, Container, Header, Icon} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <>
-      <Container>
-
-      </Container>
+        <Container>
+          <Header data-cy='game-header' >R.P.S</Header>
+          <Container data-cy='game-container'>
+            <Button.Group>
+              <Button data-cy='rock-btn' value="rock">
+                <Icon name='rock hand' size='huge' />
+                </Button>
+              <Button data-cy='scissor-btn' value="scissor">
+                <Icon name='hand scissors' size='huge'/>
+                </Button>
+              <Button data-cy='paper-btn' value="paper">
+                <Icon name='paper hand' size='huge'/>
+                </Button>
+            </Button.Group>
+          </Container>
+        </Container>
       </>
     )
   }
